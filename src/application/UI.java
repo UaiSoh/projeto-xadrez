7f7f81689ca6cb1c829524218faf0410dcbbc634
UaiSoh <joaopedroh101@gmail.com>
2024-09-16 13:27:43 -0300

    Colocando peças no tabuleiro

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,6 +4,8 @@ import chess.ChessPiece;
 
 public class UI {
 
+	
+	// Para imprimir o tabuleiro
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0; i<pieces.length; i++) {
 			System.out.print((8 - i) + " ");
